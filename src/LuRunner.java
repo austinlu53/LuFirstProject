13e11b3c94e23bcf75cfe66ea26116a3288ec783
BT_2N2_06 <sample@bths.edu>
2025-11-20 10:11:20 -0500

    added more print

--- src/LuRunner.java
+++ src/LuRunner.java
@@ -1,5 +1,7 @@
 public class LuRunner {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("CSA Class");
+        System.out.println("Techer: Mr.Miller");
     }
 }
