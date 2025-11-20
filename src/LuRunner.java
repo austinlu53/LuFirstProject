3e28b9d71dfe37aafbc18a82f04f670c48cb9c3a
BT_2N2_06 <sample@bths.edu>
2025-11-20 10:12:42 -0500

    added x's and y's and print statements

--- src/LuRunner.java
+++ src/LuRunner.java
@@ -3,5 +3,8 @@ public class LuRunner {
         System.out.println("Hello World");
         System.out.println("CSA Class");
         System.out.println("Techer: Mr.Miller");
+        int x = 5;
+        int y = 6;
+        System.out.println(x+y);
     }
 }
