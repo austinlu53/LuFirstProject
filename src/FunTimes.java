ce88265551c03e56cfe9d9883beaaa15b945bc95
BT_2N2_06 <sample@bths.edu>
2025-11-20 10:18:03 -0500

    changed LuRunner, made FunTimes

---
+++ src/FunTimes.java
@@ -0,0 +1,6 @@
+public class FunTimes {
+    public static void main(String[] args) {
+        System.out.println("a new class");
+        System.out.println("cool");
+    }
+}
--- src/LuRunner.java
+++ src/LuRunner.java
@@ -6,5 +6,6 @@ public class LuRunner {
         int x = 5;
         int y = 6;
         System.out.println(x+y);
+        System.out.println("x plus y is "+ y+x);
     }
 }
