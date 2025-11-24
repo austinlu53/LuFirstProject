8bd837b110e6504117be7f23bbf4cf86cc7f440a
BT_2N2_06 <sample@bths.edu>
2025-11-24 09:48:23 -0500

    added experiment

---
+++ src/experiment.java
@@ -0,0 +1,10 @@
+public class experiment {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+
+    }
+}
